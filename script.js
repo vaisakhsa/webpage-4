897a713943b54fbd78134f3517137721042b7925
vaisakhsa <97279263+vaisakhsa@users.noreply.github.com>
2026-07-01 10:19:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,13 @@
-// search-box open close js code
-let navbar = document.querySelector(".navbar");
-let searchBox = document.querySelector(".search-box .bx-search");
-// let searchBoxCancel = document.querySelector(".search-box .bx-x");
+let menuBtn = document.querySelector("#menu-icon");
+let closeBtn = document.querySelector("#close-icon");
+let navLinks = document.querySelector("#nav-links");
 
-searchBox.addEventListener("click", ()=>{
-  navbar.classList.toggle("showInput");
-  if(navbar.classList.contains("showInput")){
-    searchBox.classList.replace("bx-search" ,"bx-x");
-  }else {
-    searchBox.classList.replace("bx-x" ,"bx-search");
-  }
-});
+menuBtn.onclick = function () {
+  navLinks.classList.add("active");
+  document.body.style.overflow = "hidden";
+};
 
-// sidebar open close js code
-let navLinks = document.querySelector(".nav-links");
-let menuOpenBtn = document.querySelector(".navbar .bx-menu");
-let menuCloseBtn = document.querySelector(".nav-links .bx-x");
-menuOpenBtn.onclick = function() {
-navLinks.style.left = "0";
-}
-menuCloseBtn.onclick = function() {
-navLinks.style.left = "-100%";
-}
-
-
-// sidebar submenu open close js code
-let htmlcssArrow = document.querySelector(".htmlcss-arrow");
-htmlcssArrow.onclick = function() {
- navLinks.classList.toggle("show1");
-}
-let moreArrow = document.querySelector(".more-arrow");
-moreArrow.onclick = function() {
- navLinks.classList.toggle("show2");
-}
-let jsArrow = document.querySelector(".js-arrow");
-jsArrow.onclick = function() {
- navLinks.classList.toggle("show3");
-}
+closeBtn.onclick = function () {
+  navLinks.classList.remove("active");
+  document.body.style.overflow = "auto";
+};
